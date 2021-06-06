540fc53dd7d6fdfa082b87f4bde46dd96b047229
Alex Leonkov <aleonkov2001@gmail.com>
2021-06-06 13:23:32 +0300

    Init

---
+++ app.js
@@ -0,0 +1,27 @@
+const countdown = "1 september 2021"
+
+let countdowFunc = () => {
+    const countdownDate = new Date(countdown);
+    const currentDate = new Date();
+    const timeLeft = countdownDate - currentDate;
+    const days =Math.trunc(timeLeft / 86400000);
+    const hours = Math.trunc((timeLeft - days*24*3600*1000)/3600000); 
+    const minutes = Math.trunc((timeLeft - days*24*3600*1000 - hours*60*60*1000)/60000);
+    const seconds = Math.trunc((timeLeft - days*24*3600*1000 - hours*60*60*1000 - minutes* 60000) / 1000);
+    
+    const object = {
+        days,
+        hours,
+        minutes,
+        seconds
+    }
+Object.keys(object).forEach((key) => {
+document.getElementById(key).textContent = object[key]
+})
+}
+
+
+
+countdowFunc();
+setInterval(countdowFunc, 1000);
+
